3181a29e5a5b2abbc7f822583f06bde8b9411f12
Thiago W. Alves <twinkle1@stevens.edu>
2019-07-10 12:35:46 -0400

    Day 23

---
+++ Milestone1/Milestone1/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  Milestone1
+//
+//  Created by Thiago Alves on 7/9/19.
+//  Copyright © 2019 Thiago Alves. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var countryName: String?
+    var imageName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = countryName
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        if let imageToLoad = imageName {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let country = countryName else {
+            print("No image found")
+            return
+        }
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [country, image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
---
+++ Milestone1/Milestone1/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Milestone1
+//
+//  Created by Thiago Alves on 7/9/19.
+//  Copyright © 2019 Thiago Alves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var countries = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "World Flags"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix(".png") {
+                // this is a picture to load!
+                countries.append(String(item.dropLast(4)))
+            }
+        }
+        
+        countries.sort()
+        print(countries)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.imageView?.image = UIImage(named: countries[indexPath.row] + ".png")
+        cell.imageView?.layer.borderWidth = 1
+        cell.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.countryName = countries[indexPath.row]
+            vc.imageName = countries[indexPath.row] + ".png"
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
