3b1c3583f6d48606198620948c42d46be8473733
Thiago W. Alves <twinkle1@stevens.edu>
2019-07-12 12:20:01 -0400

    Small fix

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Thiago Alves on 7/12/19.
+//  Copyright © 2019 Thiago Alves. All rights reserved.
+//
+
+// swiftlint:disable identifier_name
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
